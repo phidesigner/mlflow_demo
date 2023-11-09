68a0ce69584719e61ac3bf5e57b175d58b4a6fed
Ivan Diaz <phi.designer@gmail.com>
2023-11-09 17:42:14 +0000

    Woring demo 1 step

--- 00_mlflow_pipeline/main.py
+++ 00_mlflow_pipeline/main.py
@@ -3,6 +3,16 @@ from omegaconf import DictConfig
 import mlflow
 import wandb
 
+
 @hydra.main(config_name="config")
 def main(cfg: DictConfig):
-    
+    _ = mlflow.run(
+        path='/workspaces/mlflow_demo/00_mlflow_pipeline/in/run_in.py',
+        name='main',
+        parameters={
+            'text': cfg['main']['text']
+        }
+    )
+
+    if __name__ == "__main__":
+        main()
